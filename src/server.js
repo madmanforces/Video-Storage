990bf7887b874d2e994b65df561f1015a6e456a2
madmanforces <hwangcc1019@naver.com>
2021-09-30 18:22:01 +0900

    study
    request

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
+//이곳부터 express와 관련된 코드를 작성//
+app.get("/",() => console.log("sombody is trying to go home."));
+// 이렇게 해도 됨 const handlehome = () => console.log("sombody is trying to go home.")//
+
 
 const handleListening = () => console.log(`Server listning on port http://localhost:${PORT}`);
 
