7ab891cde1588d4c711e22cfaac11ae1606c2abe
madmanforces <hwangcc1019@naver.com>
2021-09-30 18:50:36 +0900

    reespond!
    노예가 응답했다!!!

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const PORT = 4000;
 
 const app = express();
 //이곳부터 express와 관련된 코드를 작성//
-app.get("/",() => console.log("sombody is trying to go home."));
-// 이렇게 해도 됨 const handlehome = () => console.log("sombody is trying to go home.")//
+const handlehome = (req,res) => {
+    return res.send("응답해라 노예야!");
+}
 
+app.get("/",handlehome);
 
 const handleListening = () => console.log(`Server listning on port http://localhost:${PORT}`);
 
