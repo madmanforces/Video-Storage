e1065a160c2d400ffee760b5f1fd7fdca7f34b5b
madmanforces <hwangcc1019@naver.com>
2021-10-01 05:14:02 +0900

    PUG INSTALL

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,21 @@
 export const trending = (req, res) => res.send("home page videos");
-
+res.send(
+    "");
 export const see = (req, res) =>res.send("Watch");
-
+res.send(
+    "");
 export const edit = (req,res) =>res.send("Edit");
-
+res.send(
+    "");
 export const search = (res, req) => res.send("Search");
-
+res.send(
+    "");
 export const upload = (res, req) => res.send("Upload");
-
+res.send(
+    "");
 export const deletevideo = (res, req) => res.send("Delete Video");
-
+res.send(
+    "");
 
 
 
