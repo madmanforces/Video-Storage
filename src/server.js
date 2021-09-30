c2bed8e9e2c2d960ce5678bc4f0cdbdb6937bffd
madmanforces <hwangcc1019@naver.com>
2021-10-01 01:24:51 +0900

    router

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "../routers/globalRouter";
+import usersRouter from "../routers/userRouter";
+import videoRouter from "../routers/videoRouter";
+
 const PORT = 4000;
 
 const app = express();
@@ -8,30 +12,12 @@ app.use(logger);
 //이곳부터 express와 관련된 코드를 작성//
 
 //router//
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const usersRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-usersRouter.get("edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("watch", handleWatchVideo);
-
 
 app.use("/",globalRouter );
-app.use("/vidoes", videoRouter);
+
 app.use("/users", usersRouter);
 
-
+app.use("/videos", videoRouter);
 
 const home = (req,res) => {
     return res.send("hello");
