e8c292bf8f8b2e33257adcba9f66d2f9d5e6340a
madmanforces <hwangcc1019@naver.com>
2021-10-01 03:02:47 +0900

    route(export/export default)

---
+++ controllers/userController.js
@@ -0,0 +1,6 @@
+export const join = (res, req) => res.send("Join");
+
+export const edit = (res, req) => res.send("Edit User");
+
+export const remove = (res, req) => res.send("Remove User");
+
---
+++ controllers/videoController.js
@@ -0,0 +1,8 @@
+export const trending = (req, res) => res.send("home page videos");
+
+export const watch = (req, res) =>res.send("Watch");
+
+export const edit = (req,res) =>res.send("Edit");
+
+
+
