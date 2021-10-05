fadb778a9a65f0d4918bf527eb2d007e39358661
madmanforces <hwangcc1019@naver.com>
2021-10-05 18:01:05 +0900

    pug finish

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,35 +1,38 @@
 //질문하기 req,res 원래는 이거잖슴 근데 이게 왜 바뀌지?//
-
-export const trending = (req, res) =>{
- const videos = [
-     {
-         title: "First Video",
-         rating:5,
-         comments:2,
-         createdAt: "2 minutes ago",
-         views: 59,
-         id:1
-     },
-     {
-        title: "Second Video",
+let videos = [
+    {
+        title: "First Video",
         rating:5,
         comments:2,
         createdAt: "2 minutes ago",
-        views: 59,
-        id:2
+        views: 1,
+        id: 1,
     },
     {
-        title: "Third Video",
-        rating:5,
-        comments:2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id:3
-    },
- ];
- res.render("home", { pageTitle: "Home", videos });
+       title: "Second Video",
+       rating:5,
+       comments:2,
+       createdAt: "2 minutes ago",
+       views: 59,
+       id: 2,
+   },
+   {
+       title: "Third Video",
+       rating:5,
+       comments:2,
+       createdAt: "2 minutes ago",
+       views: 59,
+       id: 3,
+   },
+];
+export const trending = (req, res) => {
+ return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+    const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 
 export const edit = (req,res) =>res.send("edit");
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { see, edit, upload, deletevideo } from "../controllers/videoController";
+import { watch, edit, upload, deletevideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 
-videoRouter.get("/:id", see);
+videoRouter.get("/:id", watch);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deletevideo);
 videoRouter.get("/upload", upload);
