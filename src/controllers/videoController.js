4367d05cefb7fdd906d0fc770934ec1985961910
madmanforces <hwangcc1019@naver.com>
2021-10-09 21:38:06 +0900

    call back / promise

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,26 @@
 import video from "../models/video";
 
-export const home = (req, res) => {
-    console.log("Start");
-    video.find({}, (error, videos) => {
-      console.log("Finished");
-      return res.render("home", { pageTitle: "Home", videos });
-    });
-    console.log("I finish first");
-  };
+/*
+console.log("start")
+Video.find({}, (error, videos) => {
+  if(error){
+    return res.render("server-error")
+  }
+  return res.render("home", { pageTitle: "Home", videos });
+});
+console.log("finished")
+
+
+function안에서 return 은 아무 기능도 하지않는다. 단지 마무리라는 뜻일뿐 / 홈에서 리턴되는것은 어디어도 없다. 
+렌더는 한번만 실행할수있으므로 확실한 표현으로 리턴을 써준다
+여기선 function이 작업후 종료되게끔 리턴을 써준다 반복될수없으니까! "#function을 종료!!"
+*/
+
+export const home = async (req, res) => {
+  const videos = await video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
