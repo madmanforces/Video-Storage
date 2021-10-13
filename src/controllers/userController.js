42e6da60a119be78de8d5711aab42b22214d2968
madmanforces <hwangcc1019@naver.com>
2021-10-13 23:23:09 +0900

    status & login & join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,18 +5,19 @@ export const postJoin = async (req, res) => {
     const { name, username, email, password,password2, location } = req.body;
     const pageTitle = "Join";
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "비밀번호가 일치하지 않습니다.",
     });
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "이미 존재하는 닉네임 혹은 이메일 입니다.",
     });
   }
+  try {
     await User.create({
       name,
       username,
@@ -25,14 +26,39 @@ export const postJoin = async (req, res) => {
       location,
     });
     return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // check if password correct
+  res.end();
+};
+
 
 export const edit = (req, res) => res.send("Edit User");
 
 export const remove = (req, res) => res.send("Remove User");
 
-export const login = (req, res) => res.send("Login User");
+
 
 export const see = (req, res) => res.send("User ID");
 
 export const logout = (req, res) => res.send("Logout User");
+
+//https://ko.wikipedia.org/wiki/HTTP_%EC%83%81%ED%83%9C_%EC%BD%94%EB%93%9C 상태코드 위키백과//
