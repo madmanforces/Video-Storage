96dba2b92336a1edb64fb0cdf8386ee58a0a2162
madmanforces <hwangcc1019@naver.com>
2021-10-15 00:25:59 +0900

    env

--- src/db.js
+++ src/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/suntube", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
 });
 
 const db = mongoose.connection;
