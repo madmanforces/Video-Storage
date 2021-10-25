b0e52a435b458d037c682d5cd657420bbad178ac
madmanforces <hwangcc1019@naver.com>
2021-10-25 23:27:57 +0900

    make videoPlayer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
+// video player를 디자인하기 위해 base.pug에 있는 main.js 대신 watch.pug에 다른 js.file을 추가 해줘야함 videoPlayer.js라는 파일을 생성후 엔트리에 메인과 비디오플레이어로 나눠 저장
+// 시작점이 .entry라면 마지막에 도달점은 output이니 아웃풋에는 main.js 대신 엔트리의 이름에 맞게 작동하도록 webpack이 지원하는 [name] 이라는 변수를 써준다. 
 module.exports = {
   entry: {
     main: "./src/client/js/main.js",
